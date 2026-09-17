c7a9393656bdafccb3bc4449c01b5d0100f3df16
Unknown-Entity226 <forpcandfoxxy123@gmail.com>
2026-09-17 23:41:27 +0530

    added alembic pipeline

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from .database import engine
 from .models import posts, users
 from .routers.posts import router as posts_router
 from .routers.users import router as users_router
+from .routers.auth import router as auth_router
 
 
 posts.Base.metadata.create_all(bind = engine)
@@ -11,6 +12,7 @@ app = FastAPI()
 
 app.include_router(posts_router)
 app.include_router(users_router)
+app.include_router(auth_router)
 
 @app.get(path="/")
 def home()->dict:
